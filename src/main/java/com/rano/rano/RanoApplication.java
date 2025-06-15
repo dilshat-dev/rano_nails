aa8f62f7d45a746aea2b3f3aea1b3b524cda5e15
Dima <dilsh.dev01@gmail.com>
2025-06-15 18:07:42 +0500

    init

---
+++ src/main/java/com/rano/rano/RanoApplication.java
@@ -0,0 +1,13 @@
+package com.rano.rano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanoApplication.class, args);
+	}
+
+}
